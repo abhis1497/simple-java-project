1071655f1dfd8f7dce84cdd6a2f466daaeeb55e3
munshinuwed <nuwed.munshi@gmail.com>
2019-11-30 12:54:30 +0530

    Vehicle and User added.

--- src/main/java/com/learning/test/App.java
+++ src/main/java/com/learning/test/App.java
@@ -1,13 +1,30 @@
 package com.learning.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.learning.test.model.User;
+import com.learning.test.model.Vehicle;
+import com.learning.test.service.UserService;
+import com.learning.test.service.VehicleService;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		/*
+		 * UserService userService = new UserService(); List<User> userList = new
+		 * ArrayList<>(); userService.createUser(new User(), userList);
+		 * //userService.createUser(new User(), userList); // userService.createUser(new
+		 * User(), userMap); // userService.createUser(new User(), userMap);
+		 * //userService.display(1, userList); // userService.deleteUser(2,userList);
+		 * //userService.updateUser(0, userList); userService.displayAll(userList);
+		 */
+		VehicleService vehicleservice = new VehicleService();
+		List<Vehicle> vehicleList = new ArrayList<>();
+		vehicleservice.createVehicle(new Vehicle(), vehicleList);
+		vehicleservice.displayAll(vehicleList);
+	}
 }
